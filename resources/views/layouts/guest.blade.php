aaf3f6f4436e1f2ecf040806324fb93c906945a9
Fio Juliansyah <63538861+fiojuliansyah@users.noreply.github.com>
2025-08-01 11:14:41 +0700

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <!-- Favicon icon -->
-    <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/favicon.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/fav.png">
     <title>GIHUD.GG | Live HUD Management</title>
     <!-- Custom CSS -->
     
